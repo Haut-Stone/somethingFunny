2ce408528ba6d0dd24961e3f120c4be357700f12
Haut-Stone <haut.1604.stone@gmail.com>
2017-01-17 22:48:55 +0800

    一首小诗
    代码入诗，一样优雅

---
+++ poem.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Author: Haut-Stone
+# @Date:   2017-01-17 21:34:25
+# @Last Modified by:   Haut-Stone
+# @Last Modified time: 2017-01-17 21:38:50
+
+while gress.BearItsSeed #草在结它的种子
+	and wind.ShakeItsLeaves():#风在摇它的叶子
+	we.StandingUp()#我们站着
+	we.Silent()#不说话
+
+	everything.Fine();#就十分美好
+#----选自顾城《门前》
