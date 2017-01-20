9f1b4bedf605a5f27c1a4aca4cdf0aa26519fad0
Haut-Stone <haut.1604.stone@gmail.com>
2017-01-20 19:36:32 +0800

    update

---
+++ -->.cpp
@@ -0,0 +1,36 @@
+/*
+* Created by ShiJiahuan(li) in haut.
+* for more please visit www.shallweitalk.com
+* 
+* Copyright 2016 SJH. All rights reserved.
+*
+* @Author: Haut-Stone
+* @Date:   2017-01-19 17:51:54
+* @Last Modified by:   Haut-Stone
+* @Last Modified time: 2017-01-19 17:58:39
+*/
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+using namespace std;
+
+//在c++中有一个传说，大学里学习的运算符是不完整的，有一些鲜为人知的运算符不为人知。
+//你可能会说是异或。。。
+//不不不
+//少年你太天真了23333
+//不多说，上代码 talk is cheap show me the code
+
+
+int main(void)
+{
+	int i=100;
+	while(0 <------ i){
+		printf("%d\n", i);
+	}
+	//运行下试试？
+	//这就是趋近于运算符，箭头的长度代表了趋近的速度。
+	//怎么样，amazing 吧。。2333333
+    return 0;
+}
