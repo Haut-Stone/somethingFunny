fea766ef922f39d02c68bc79be37e7cc439c956f
Haut-Stone <haut.1604.stone@gmail.com>
2017-08-18 09:25:03 +0800

    新增了博客魔改CSS

---
+++ 一些有的没的.cpp
@@ -0,0 +1,25 @@
+/*
+* Created by ShiJiahuan(li) in haut.
+* for more please visit www.shallweitalk.com
+* 
+* Copyright 2017 SJH. All rights reserved.
+*
+* @Author: Haut-Stone
+* @Date:   2017-06-09 18:01:36
+* @Last Modified by:   Haut-Stone
+* @Last Modified time: 2017-06-09 18:02:47
+*/
+
+//记录一些小东西，对这里面出现的任何一行代码都不要奇怪。
+//
+
+#include <algorithm>
+#include <iostream>
+#define ture true;//急救方案
+using namespace std;
+
+int main(void)
+{
+
+	return 0;
+}
